ce3f657aed7663a968e43a63f83a2fba01b924d9
Tyler Black Hernandez <tylerblackhernandez@gmail.com>
2017-02-15 10:59:16 -0600

    add validations to models and database. Update styles * add uniqueness validations to database for brand and category * add product brand and category validations to models * made devise views purty

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,18 @@ class Product < ApplicationRecord
                     default_url: "missing_:style.jpg"
 
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
+  validates_presence_of :name, :price, :quantity, :description, :brand, :category
+
+  validates_numericality_of :price, greater_than_or_equal_to: 0.01
+
+  validates_numericality_of :quantity, greater_than_or_equal_to: 0
+  
+  # Fields
+  # name - presence
+  # description - presence
+  # category - presence
+  # brand - presence
+  # price - >= 0.01
+  # quantity >= 0
 end
