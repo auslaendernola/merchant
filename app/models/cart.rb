a440216a97a75c1a786c7036c18cf733b8fb04ef
Tyler Black Hernandez <tylerblackhernandez@gmail.com>
2017-02-21 09:07:48 -0600

    create relationships between line items, orders and users, add order model

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -5,7 +5,7 @@ class Cart < ApplicationRecord
   # Add new Line Item if Product is not in Cart
   def add_product(product_id)
     product = Product.find(product_id)
-    current_item = @cart.add_product(product.id)
+    current_item = line_items.find_by(product_id: product_id)
 
     if current_item
       current_item.quantity += 1
